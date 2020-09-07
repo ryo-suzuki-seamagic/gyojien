e13a44871e7035f88fe105b608cdcdc832ed9351
ryo-suzuki-seamagic <r40suzu@gmail.com>
2020-09-07 18:48:36 +0900

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :fishcatchs
+  has_many :fishcathccomenst
+  has_many :recipes
+  has_many :recipecomments
+  has_many :colimus
+
+
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true
+    validates :location
+    validates :first_name
+    validates :last_name
+    validates :password, format:{ with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/ },
+                         confirmation: true
+  end
 end
